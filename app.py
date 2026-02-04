3c39bb178eaf5f2abab10d1c1898cb1878fc9077
hui9-alt <huihui3039@gmail.com>
2026-02-04 23:36:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ div.stButton {
 }
 
 div.stButton {
-    margin-top: -200px;
+    margin-top: -150px;
     transform: translateX(3000px);  /* +で右 / -で左 */
 }
 
@@ -151,6 +151,7 @@ if st.session_state.draft_text:
 {body}"""
 
     st.code(full_output, language="markdown")
+
 
 
 
